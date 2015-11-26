86e56b8554ea4fadd584e3bf9c58de4941267a5b
Thiago Rodrigues Colucci <thiago.rodrigues-colucci@klarna.com>
2015-11-26 20:19:59 +0100

    translating texts from language to target language

--- web/models/translator.ex
+++ web/models/translator.ex
@@ -1,6 +1,14 @@
 defmodule MeiPortugueseBot.Translator do
 
   def translate(from, to, text) do
+    token = fetch_token
+    {:ok, response} = HTTPoison.get(
+      "http://api.microsofttranslator.com/v2/Http.svc/Translate",
+      [{:Authorization, "Bearer " <> token.access_token}],
+      [params: [from: from, to: to, text: text]]
+    )
+    [[_, translated]] = Regex.scan(~r/>([^<]+)</, response.body)
+    translated
   end
 
   def fetch_token do
@@ -8,7 +16,7 @@ defmodule MeiPortugueseBot.Translator do
     client_secret = System.get_env("CLIENT_SECRET")
 
     {:ok, response} = HTTPoison.post(
-      'https://datamarket.accesscontrol.windows.net/v2/OAuth2-13',
+      "https://datamarket.accesscontrol.windows.net/v2/OAuth2-13",
       {:form, [{:client_id, client_id},
                {:client_secret, client_secret},
                {:scope, 'http://api.microsofttranslator.com'},
